ddc83cea20a48e16090716e2628489f9b4e82e38
Buckminsterfullerene <spam12345@ommerick.net>
2026-01-25 15:50:31 -0500

    CSS Responsive

--- src/app/layout/left-sidebar/left-sidebar-state.ts
+++ src/app/layout/left-sidebar/left-sidebar-state.ts
@@ -7,9 +7,13 @@ import { Breakpoint } from '../../services/breakpoint';
 export class LeftSidebarState {
   #breakpoint = inject(Breakpoint);
 
+  readonly #EXPANDED_WIDTH_PX = 250;
+  readonly #COLLAPSED_WIDTH_PX = 60;
+  readonly #HIDDEN_WIDTH_PX = 0;
+
   // Base state
-  readonly isOpen = signal(true);
-  readonly width = signal(250); // in pixels
+  readonly isOpen = signal(!this.#breakpoint.isMobile());
+  readonly width = signal(this.#breakpoint.isMobile() ? this.#HIDDEN_WIDTH_PX : this.#EXPANDED_WIDTH_PX); // in pixels
 
   toggle(): void {
     this.isOpen.update(v => {
@@ -17,8 +21,9 @@ export class LeftSidebarState {
 
       if (this.#breakpoint.isMobile()) {
         // On mobile, sidebar is either fully open (250px) or fully closed (0px)
-        this.width.set(newState ? 250 : 0);
-      // this.width.set(newState ? 250 : 60);
+        this.width.set(newState ? this.#EXPANDED_WIDTH_PX : this.#HIDDEN_WIDTH_PX);
+      } else {
+        this.width.set(newState ? this.#EXPANDED_WIDTH_PX : this.#COLLAPSED_WIDTH_PX);
       }
       return newState;
     });
@@ -27,14 +32,12 @@ export class LeftSidebarState {
   close(): void {
     this.isOpen.set(false);
     if (this.#breakpoint.isMobile()) {
-      this.width.set(0);
+      this.width.set(this.#HIDDEN_WIDTH_PX);
     }
   }
 
   open(): void {
     this.isOpen.set(true);
-    if (!this.#breakpoint.isMobile()) {
-      this.width.set(250);
-    }
+    this.width.set(this.#EXPANDED_WIDTH_PX);
   }
 }
